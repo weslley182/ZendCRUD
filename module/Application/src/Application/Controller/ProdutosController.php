fd0f76a49415668c6e2d7f78ccd96d5cd7e4e4df
Wesley Machado <weslley182@gmail.com>
2016-03-20 22:45:44 -0300

    primeiro commit

---
+++ module/Application/src/Application/Controller/ProdutosController.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Wesley
+ * Date: 20-Mar-16
+ * Time: 20:03
+ */
+
+namespace Application\Controller;
+
+use Application\Form\ProdutoForm;
+use Zend\View\Model\ViewModel;
+use Zend\Mvc\Controller\AbstractActionController;
+
+class ProdutosController extends AbstractActionController{
+    public function indexAction(){
+
+    }
+
+    public function cadastrarAction(){
+        $form = new ProdutoForm();
+    }
+
+}
---
+++ module/Application/src/Application/Form/ProdutoForm.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Wesley
+ * Date: 20-Mar-16
+ * Time: 22:10
+ */
+namespace Application\Form;
+
+use Zend\Form\Element\Checkbox;
+use Zend\Form\Element\File;
+use Zend\Form\Element\Hidden;
+use Zend\Form\Element\Text;
+use Zend\Form\Element\Textarea;
+use Zend\Form\Form;
+
+class ProdutoForm extends Form{
+    public function __construct(){
+        parent::__construct('produto');
+
+        $id = new Hidden('id');
+        $nome = new Text('nome');
+        $nome->setLabel('Nome:')
+            ->setAttributes([
+                'style' => 'width:150px;'
+            ]);
+        $preco = new Text('preco');
+        $preco->setLabel('Preço:')
+            ->setAttributes([
+                'style' => 'width:60px;'
+            ]);
+        $foto = new File();
+        $foto->setLabel('Foto:')
+            ->setAttributes([
+                'style' => 'width:150px;'
+            ]);
+
+        $descricao = new Textarea('descricao');
+        $descricao->setLabel('Descricao')
+            ->setAttributes([
+                'style' => 'width:150px; heigth:100px;'
+            ]);
+
+        $status = new Checkbox();
+        $status->setLabel('Status:')
+            ->setValue(1);
+
+        $submit = new Button();
+        $submit->setLabel('Cadastrar')
+            ->setAttributes([
+                'type' => 'submit'
+            ]);
+
+        $this->add($id);
+        $this->add($nome);
+        $this->add($preco);
+        $this->add($foto);
+        $this->add($descricao);
+        $this->add($status);
+        $this->add($submit);
+
+    }
+}
